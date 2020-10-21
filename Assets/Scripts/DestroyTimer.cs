e34b17e385bcb35ce9d9c35e33c83ab855c9af39
x2Jamille <x2Jamille@users.noreply.github.com>
2020-10-21 10:23:56 +0300

    Init

---
+++ Assets/Scripts/DestroyTimer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyTimer : MonoBehaviour
+{
+    private void Awake()
+    {
+        Destroy(gameObject, 10);
+    }
+}
---
+++ Assets/Scripts/EntitySpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntitySpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _prefab;
+
+    private Transform[] _spawners;
+    private int _nextSpawnerIndex;
+    private float _spawnDelay = 2f;
+
+    void Awake()
+    {
+        _spawners = GetComponentsInChildren<Transform>();
+        StartCoroutine(SpawnWithDelay());
+    }
+
+    private IEnumerator SpawnWithDelay()
+    {
+        while (true)
+        {
+            if (!_spawners[_nextSpawnerIndex].Equals(transform))
+            {
+                Instantiate(_prefab, _spawners[_nextSpawnerIndex].position, Quaternion.identity);
+                yield return new WaitForSeconds(_spawnDelay);
+            }
+
+            _nextSpawnerIndex++;
+            if (_nextSpawnerIndex >= _spawners.Length)
+                _nextSpawnerIndex = 0;
+            
+        }
+    }
+}
